c4386ff8030cc7de6acd7afee4d3c22c0253f06e
Zhen Zhang <izgzhen@gmail.com>
2015-09-28 17:22:49 +0800

    Canonical

--- OSS.hs
+++ OSS.hs
@@ -1,4 +1,4 @@
-{-# LANGUAGE OverloadedStrings, FlexibleInstances #-}
+{-# LANGUAGE OverloadedStrings, FlexibleInstances, FlexibleContexts #-}
 
 module OSS where
 
@@ -6,6 +6,10 @@ import qualified Data.ByteString.Char8 as BC
 -- import qualified Data.ByteString as BS
 import Auth
 import Network.HTTP.Base (RequestMethod)
+import Data.Char
+import Data.List
+import System.FilePath
+import qualified Data.Map as M
 
 data Content = Content {
   _text :: BC.ByteString
@@ -23,17 +27,42 @@ constructSign verb content secret expires ossHeaders resource = base64 (hmacSha1
                 canonicalize ossHeaders ++
                 canonicalize resource
 
-
 class Canonicalizable a where
     canonicalize :: a -> String
 
 
-data OSSHeader = X_OSS_Meta_Name
+type OSSHeader = (String, String) -- Example ("X-OSS-Meta-Name", "Taobao")
 
-data OSSResource = OSSResource
+data OSSResource = Bucket String (Maybe (String, SubResource))
+
+type SubResource = [(String, Maybe String)] -- KVa
 
 instance Canonicalizable [OSSHeader] where
-    canonicalize headers = "headers"
+    canonicalize = unlines . map p
+                           . sortBy (\(k1, _) (k2, _) -> compare k1 k2)
+                           . M.toList
+                           . foldl (\hs h -> h `insertIn` hs) M.empty
+                           . map (\(k, v) -> (map toLower k, v))
+        where
+            p :: (String, [String]) -> String
+            p (k, vs) = k ++ ":" ++ (map (\c -> if c == ' ' then ',' else c) (unwords vs))
+
+            insertIn :: (String, String) -> M.Map String [String] -> M.Map String [String]
+            insertIn (k, v) hs = case M.lookup k hs of
+                Just vs -> M.insert k (v:vs) hs
+                Nothing -> M.insert k [v] hs
 
 instance Canonicalizable OSSResource where
-    canonicalize resource = "resource"
+    canonicalize (Bucket bucketName maybeObj) = "" </> bucketName </> p maybeObj
+        where
+            p Nothing = ""
+            p (Just (objName, maybeSubResource)) = objName ++ p' maybeSubResource
+            p' []     = ""
+            p' (r:[]) = p'' r
+            p' (r:subr) = "?" ++ foldl (\s r -> s ++ "&" ++ p'' r) (p'' r) subr
+            p'' (k, Nothing) = k
+            p'' (k, Just v)  = k ++ "=" ++ v
+
+
+
+
